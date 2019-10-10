88bb6df26f0453b777a0b17fb515b7e5835a58dc
Bug-Breeder <591584644@qq.com>
2019-10-10 18:37:54 +0800

    littlefunction

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed = 5f;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.SetBool("isIdle", true);
+        anim.SetBool("isMid", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //移动相关
+        float movement = Input.GetAxis("Horizontal") * Time.deltaTime * movementSpeed;
+        if (movement != 0&& anim.GetCurrentAnimatorStateInfo(0).IsName("isAttack") == false)
+        {
+            anim.SetBool("isRun", true);
+            anim.SetBool("isIdle", false);
+            // 如果movement值大于0，这表示玩家对象向右移动，所以将玩家对象向右转并进行移动
+            if (movement > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+                transform.Translate(transform.right * movement);
+            }
+            else if (movement < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+                transform.Translate(transform.right * movement);
+            }
+        }
+        else if (movement == 0 && anim.GetCurrentAnimatorStateInfo(0).IsName("isAttack") == false)
+        {
+            anim.SetBool("isRun", false);
+            anim.SetBool("isIdle", true);
+        }
+        //剑位置
+        if(Input.GetKeyDown(KeyCode.S))//向下调剑的位置
+        {
+            if(anim.GetBool("isUp"))//如果在上剑位
+            {
+                anim.SetBool("isMid", true);
+                anim.SetBool("isUp", false);
+            }
+            else if(anim.GetBool("isMid"))//在中剑位
+            {
+                anim.SetBool("isLow", true);
+                anim.SetBool("isMid", false);
+            }//本来在低剑位不作调整
+        }
+        else if(Input.GetKeyDown(KeyCode.W))//向上调剑的位置
+        {
+            if(anim.GetBool("isLow"))//在低剑位
+            {
+                anim.SetBool("isMid", true);
+                anim.SetBool("isLow", false);
+            }
+            else if(anim.GetBool("isMid"))//中剑位
+            {
+                anim.SetBool("isUp", true);
+                anim.SetBool("isMid", false);
+            }
+        }
+        //攻击相关
+        if (Input.GetKey(KeyCode.J))//J键攻击
+        {
+            anim.SetBool("isAttack", true);
+            anim.SetBool("isIdle", false);
+        }
+        else
+        {
+            anim.SetBool("isAttack", false);
+        }
+    }
+}
